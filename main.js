3d91d8d5a81b44bec629c9da6bdef59cff820645
AhmedBouafif <ahmedbouafif500@gmail.com>
2021-10-10 19:06:58 +0100

    on progressssssssseeeee

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-var users = []
+var nameArray=["ahmed","sakr","sarra","walid","bob","halim","foued"]
 $("#bob").on("click",function(){
-    localStorage.setItem("firstname",$('#fname').val())
-    localStorage.setItem("lastname",$('#lname').val())
-    localStorage.setItem("age",$('#age').val())
-    localStorage.setItem("email",$('#email').val())
-    localStorage.setItem("password",$('#password').val())
+
 
     var user = {}
-    user.firstname=localStorage.getItem("firstname")
-    user.lastname=localStorage.getItem("lastname")
-    user.age=localStorage.getItem("age")
-    user.email=localStorage.getItem("email")
-    user.password=localStorage.getItem("password")
-    var userAccount={}
-    userAccount[localStorage.getItem("firstname")]=user
-    localStorage.setItem(localStorage.getItem("firstname"),JSON.stringify(userAccount))
-});
-var nameArray=["ahmed","sakr","sarra","walid"]
+    user.firstname=$('#fname').val()
+    user.lastname=$('#lname').val()
+    user.age=$('#age').val()
+    user.email=$('#email').val()
+    user.password=$('#password').val()
+    var usersArray = JSON.parse(localStorage.getItem("users"))|| []
+    usersArray.push(user)
+    localStorage.setItem("users" , JSON.stringify(usersArray))
+ });
 function createUsers(nameArray) {
+    nameArray.push(localStorage.getItem("firstname"))
     var res=[]
     for (let i = 0; i < nameArray.length; i++) {
         res.push(JSON.parse(localStorage.getItem(nameArray[i])))
     }
     return res
 }
-
+function checkuser(){ 
+    var bob = JSON.parse(localStorage.getItem("users"))
+    console.log(bob)
+    for(i=0;i<bob.length;i++){
+        if($('#ffname').val()===bob[i].firstname && $('#ppassword').val()===bob[i].password){
+            window.location.replace("./home.html"); 
+        }
+    }
+    window.location.replace("./register.html");
+}
 
   
