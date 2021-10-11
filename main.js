9f1636ad6219d697e6e76e3fa22a2c09796506d3
AhmedBouafif <ahmedbouafif500@gmail.com>
2021-10-11 03:12:26 +0100

    finished

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var nameArray=["ahmed","sakr","sarra","walid","bob","halim","foued"]
+var nameArray=[]
 $("#bob").on("click",function(){
 
 
@@ -12,23 +12,40 @@ $("#bob").on("click",function(){
     usersArray.push(user)
     localStorage.setItem("users" , JSON.stringify(usersArray))
  });
-function createUsers(nameArray) {
-    nameArray.push(localStorage.getItem("firstname"))
-    var res=[]
-    for (let i = 0; i < nameArray.length; i++) {
-        res.push(JSON.parse(localStorage.getItem(nameArray[i])))
+ var nameArray = JSON.parse(localStorage.getItem("users"))
+$("#bob1").on("click",function(){ 
+    for(i=0;i<nameArray.length;i++){
+        if($("#ffname").val()===nameArray[i].firstname){
+            if($("#ppassword").val()===nameArray[i].password){
+                window.location.replace("file:///C:/Users/Lenovo/Desktop/myProject/home.html")
+            }else{
+            alert("your password is wrong")
+            } 
+            break;
+        }else if(($("#ffname").val()==="")&&($("#ppassword").val()==="")){
+            alert("please put you name and password")
+            break;
+        }else{ 
+            window.location.replace("file:///C:/Users/Lenovo/Desktop/myProject/register.html")
+        alert("you have to creat an account please")
+        break;
     }
-    return res
 }
-function checkuser(){ 
-    var bob = JSON.parse(localStorage.getItem("users"))
-    console.log(bob)
-    for(i=0;i<bob.length;i++){
-        if($('#ffname').val()===bob[i].firstname && $('#ppassword').val()===bob[i].password){
-            window.location.replace("./home.html"); 
-        }
-    }
-    window.location.replace("./register.html");
-}
+})
 
+$("#bob").on("click",function(){
+    if(($("#fname").val()==="")||
+    ($("#lname").val()==="")||
+    ($("#age").val()==="")||
+    ($("#email").val()==="")||
+    ($("#password").val()==="")){
+        alert("please fill all of you informations")
+    }else{
+        window.location.replace("file:///C:/Users/Lenovo/Desktop/myProject/home.html")
+    }
+})
+
+$("#account").on("click",function(){
+    window.location.replace("file:///C:/Users/Lenovo/Desktop/myProject/login.html")
+})
   
